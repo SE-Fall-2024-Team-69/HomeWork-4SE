bf38156c06ac53a9da00c9003d0a1236ce2f056d
yaswanth67 <66238314+yaswanth67@users.noreply.github.com>
2024-10-10 12:28:40 -0400

    task 3

--- task3.sh
+++ task3.sh
@@ -1,6 +1,24 @@
+
 #!/bin/bash
 
 # 1.Extract passengers from 2nd class who embarked at Southampton.
 # 2.Replace male/female with M/F.
 # 3.Calculate average age of the filtered passengers
-
+gawk -F, '
+    NR > 1 && $3 == 2 && $12 == 'S' {
+        sex = ($6 == "male") ? "M" : "F";
+        age = $7;
+        if (age != "") {
+            sum += age; 
+            count++;
+        }
+    } 
+    END {
+        if (count > 0) {
+            avg = sum / count;  
+            print "Average age: " avg; 
+        } else {
+            print "Age Unavailable!"
+        }
+    }
+' titanic.csv
