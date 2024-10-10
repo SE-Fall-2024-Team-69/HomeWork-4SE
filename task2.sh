a312fa749f6118819c35dcbd16a91e6995dd652b
Yaswanth <55339925+YaswanthMullamuri@users.noreply.github.com>
2024-10-10 15:18:57 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,3 +1,9 @@
 # Count "CSC510" occurrences in files containing "sample"
-
-
+grep -rl "sample" . | while read file; do
+    count=$(grep -o "CSC510" "$file" | wc -l)
+    # MacOS does not support -c%s to get this file size.
+    size=$(stat -f%z "$file")
+    if [ "$count" -ge 3 ]; then
+        echo "$count $size $file"
+    fi
+done | sort -k1,1nr -k2,2nr | gawk '{sub(/file_/, "filtered_", $3); print $3}'
