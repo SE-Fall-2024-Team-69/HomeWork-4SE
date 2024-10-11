b5698aae8cd1ed17607d09af20e42f6275002e59
yaswanth67 <66238314+yaswanth67@users.noreply.github.com>
2024-10-10 23:01:02 -0400

    Add files via upload

--- task3.sh
+++ task3.sh
@@ -4,21 +4,16 @@
 # 1.Extract passengers from 2nd class who embarked at Southampton.
 # 2.Replace male/female with M/F.
 # 3.Calculate average age of the filtered passengers
-gawk -F, '
-    NR > 1 && $3 == 2 && $12 == 'S' {
-        sex = ($6 == "male") ? "M" : "F";
-        age = $7;
-        if (age != "") {
-            sum += age; 
-            count++;
-        }
-    } 
-    END {
-        if (count > 0) {
-            avg = sum / count;  
-            print "Average age: " avg; 
-        } else {
-            print "Age Unavailable!"
-        }
-    }
-' titanic.csv
+#!/bin/bash
+
+
+awk -F',' 'BEGIN {OFS=","} $3 == 2 && $NF ~ /S/ {print $0}' titanic.csv |
+sed 's/female/F/g; s/male/M/g' | 
+awk -F',' 'BEGIN {OFS=","} {print $0} {if($7 != "") {sum += $7; count++}}
+END {
+if(count > 0) {
+Avg_age =sum/count; print "Average Age : " Avg_age
+} else {
+print "No age found"
+}
+}'
